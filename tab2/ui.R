06b31e26732e282f7765c9c528188589785f1fd5
strawhatfamus <strawhatfamus@gmail.com>
2016-12-08 15:58:03 -0800

    new

--- tab2/ui.R
+++ tab2/ui.R
@@ -17,15 +17,15 @@ source("../tab2working/tab2script.r")
 shinyUI(navbarPage(inverse = TRUE, 'Presidential Election 2016',
                    #Panel Page
                    
-                   tabPanel('Predicting Election',
+                   tabPanel('Election Distribution',
                             #Title page
-                            titlePanel('Mapping a Change in the Results'),
+                            titlePanel('Changing the Election Results'),
                             sidebarLayout(
                               #sidebars
                               sidebarPanel(
                                 #Dropdown Menu
                                 selectInput('state', label = 'Choose a State', choices = as.list(states_name_location$region), selected = "normal"),
-                               
+                                
                                 #Interactive UI Output
                                 uiOutput("select1"),
                                 
@@ -37,12 +37,14 @@ shinyUI(navbarPage(inverse = TRUE, 'Presidential Election 2016',
                               #Exporting the input value in tabs
                               mainPanel(
                                 tabsetPanel(type = "tabs",
-                                            tabPanel("Distribution Map by Counties", plotOutput("map")),
-                                            tabPanel("Popular Votes Histogram", plotlyOutput("chart"))
+                                            tabPanel("Distribution Map by Counties", p("The following map shows the distribution of presidential election votes by county. You can see the default data by choosing default, or alter the results by changing the state and its counties."), 
+                                                     plotOutput("map")),
+                                            tabPanel("Popular Votes Histogram", p("The followig histogram shows the balance of votes for both candidates. Choose default to view the overall balance, or alter the results by choosing a state and county, hence seeing the altered balance within that state."), 
+                                                     plotlyOutput("chart"))
                                 )
                               )
                             )
                    )
                    
-                  
+                   
 ))
