d7ffebef50fa331ed7613360987dc0f85e28c4d1
soravassi <andresc7@uw.edu>
2016-12-08 16:23:05 -0800

    -script

--- tab3/server.R
+++ tab3/server.R
@@ -8,18 +8,7 @@ library(tidyr)
 # Define server logic 
 shinyServer(function(input, output) {
    
-   #Reads data from the 2016 general presidential election into a dataframe
-   election <- read.csv('data/presidential_general_election_2016.csv', stringsAsFactors = FALSE)
-    election <- select(election, electoral_votes, rank, name, state, votes, vote_pct) %>% 
-   filter(electoral_votes != 107) %>% 
-      arrange(state)
-    #Creates dataframe "winner" by filtering only the winners of the election in each state,
-    #selects the colums related to the winning candidate name and electoral votes for the given
-    #state
-    winner <- group_by(election, state) %>% 
-      filter(rank == 1) %>% 
-      select(name, electoral_votes) %>% 
-      arrange(state)
+ 
     
    
       
