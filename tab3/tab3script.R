9f25c66976ca3f35d4b3e37a46e485337983cb1e
soravassi <andresc7@uw.edu>
2016-12-08 16:22:23 -0800

    updated tab3script

--- tab3/tab3script.R
+++ tab3/tab3script.R
@@ -1,13 +1,19 @@
-election <- read.csv('data/presidential_general_election_2016.csv', stringsAsFactors = FALSE)
 
-#Creates dataframe "winner" by filtering only the winners of the election in each state,
-#selects the colums related to the winning candidate name and electoral votes for the given
-#state
-winner <- group_by(election, state) %>% 
-  filter(rank == 1) %>% 
-  select(name, electoral_votes) 
+#Reads data from the 2016 general presidential election into a dataframe
+   election <- read.csv('data/presidential_general_election_2016.csv', stringsAsFactors = FALSE)
+    election <- select(election, electoral_votes, rank, name, state, votes, vote_pct) %>% 
+   filter(electoral_votes != 107) %>% 
+      arrange(state)
+    #Creates dataframe "winner" by filtering only the winners of the election in each state,
+    #selects the colums related to the winning candidate name and electoral votes for the given
+    #state
+    winner <- group_by(election, state) %>% 
+      filter(rank == 1) %>% 
+      select(name, electoral_votes) %>% 
+      arrange(state)
 
 choice <- group_by(election, state) %>% 
   filter(rank == 1) %>% 
   select(name, electoral_votes) %>% 
-  mutate(selected = 1)
+  mutate(selected = 1)%>%
+  arrange(state)
